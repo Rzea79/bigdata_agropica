76dd6944d275a6a6240df08de7bf8530947f45b3
rzea79 <rzea27@gmail.com>
2021-01-10 12:29:58 -0400

    Subida de Dasboard a Nube #1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,30 @@
-@extends('layouts.app')
+@extends('adminlte::page')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+ @section('title','DashBoard') 
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+ {{--  --}}
 
-                    {{ __('You are logged in!') }}
-                </div>
+@section('content_header')
+    <div class="card">
+        <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">Hola mundo</h1>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat reprehenderit, cum rerum aperiam iste soluta, magnam quas blanditiis saepe voluptates aliquid ipsa voluptatum! Quia repellendus eveniet officia? A, ab alias?</p>
             </div>
         </div>
     </div>
-</div>
-@endsection
+@stop
+
+@section('content')
+    <p> Welcome to this beautifull  admin panel </p>
+@stop
+
+@section('css')
+    <link rel= "stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> Console.log('Hi!';) </script>
+@stop
